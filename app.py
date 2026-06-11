1faeb1ac36b77022b8cf0b57094da284ec00c512
nolr <nolr3@naver.com>
2026-06-11 12:01:01 +0900

    Auto commit: 2026-06-11 12:01 - 저장됨

--- app.py
+++ app.py
@@ -465,6 +465,6 @@ if (map_data or lob != st.session_state["previous_lob"]) and map_data.get("last_
         elif survival_days/365>4:
             st.warning(f"⚠️ **해당 구역의 소상공인 평균 생존기간: {(survival_days-200)/365:.1f}~{(survival_days+200)/365:.1f}년**")
         else:
-            st.error(f" **해당 구역의 소상공인 평균 생존기간: {(survival_days-200)/365:.1f}~{(survival_days+200)/365:.1f}년**")
+            st.error(f"☠️ **해당 구역의 소상공인 평균 생존기간: {(survival_days-200)/365:.1f}~{(survival_days+200)/365:.1f}년**")
         
         
